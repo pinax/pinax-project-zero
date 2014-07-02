cda9dd9e73962d9ae98bdbed6e01cde8de787d53
James Tauber <jtauber@jtauber.com>
2014-07-02 00:43:21 -0400

    removed long commented-out line

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
-# handler500 = "pinax.views.server_error"
-
 
 urlpatterns = patterns("",
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
