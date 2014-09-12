7a057a86890a71fe4580b539670b70c2821bbab3
Brian Rosner <brosner@gmail.com>
2014-09-12 07:09:59 -0600

    initial Django 1.7 support

--- project_name/__init__.py
+++ project_name/__init__.py
@@ -4,3 +4,5 @@ __about__ = """
 This project lays the foundation for all other Pinax starter projects. It
 provides the project directory layout and bootstrap-based theme.
 """
+
+default_app_config = "{{ project_name }}.apps.AppConfig"
