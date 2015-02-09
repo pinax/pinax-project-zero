c65991316869399407fa4478fb1e9e3f312f8183
Patrick Altman <paltman@gmail.com>
2015-02-08 19:32:05 -0600

    Add CI to PPZ

--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
