9c5e60b0ba917863e9cc37f451d08b4f6913d9b2
Shaun <shaun.guarini@gmail.com>
2015-02-21 13:53:53 -0500

    import admin, added url

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from django.contrib import admin
 
 
 urlpatterns = patterns(
     "",
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r'^admin/', include(admin.site.urls)),
 )
